2ed156d14a91ca2817f1753f3aa77404d88306dc
S.Haturusinghe <tothegoatz@gmail.com>
2020-10-20 23:54:42 +0530

    iframe size change responsive

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 const open_btn = document.getElementById("open-trailer");
 const model_container = document.getElementById("trailer_container");
+const model_container_div = document.getElementById("trailer_modal");
 const close_btn = document.getElementById("close");
 const yt_vid = document.getElementById("yt_trailer");
 
 open_btn.onclick = function () {
+  yt_vid.width = model_container_div.offsetWidth - 10;
   console.log("Show trailer");
   model_container.classList.add("show");
 };
